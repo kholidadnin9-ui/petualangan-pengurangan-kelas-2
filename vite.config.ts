9e9bc947d91cf28863110d0c9baff21d40a524a7
kholidadnin9-ui <kholidadnin9@gmail.com>
2026-09-18 23:42:37 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/petualangan pengurangan kelas 2/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
